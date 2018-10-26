0df02eb22430640353e071d808022fb669959780
Mona Bocharova <bocharova.mona@gmail.com>
2018-10-26 15:06:49 -0700

    readying files for PR

--- client/src/components/MortgageSection.jsx
+++ client/src/components/MortgageSection.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 
 const MortgageSection = (props) => (
-    <h2 className="collapsible" onClick={props.onClick}>Mortgage</h2>
+        <div className="title" onClick={props.onClick}>
+            <span className="collapsible">Mortgage </span><i className="fas fa-angle-up" id="icon-angle"></i>
+        </div>
 );
+
 
 export default MortgageSection;
